c15be3cea5665fdc69855f0f90994125548c4e84
fazlurnu <fazlur.rahman.ae@gmail.com>
2019-08-02 09:48:57 +0200

    Add Override via Teleop

--- scripts/control.py
+++ scripts/control.py
@@ -6,12 +6,27 @@ from std_msgs.msg import Float64
 from mavros_msgs.msg import OverrideRCIn
 from mavros_msgs.msg import RCIn
 from mavros_msgs.srv import SetMode, CommandBool, CommandTOL
+from sensor_msgs.msg import Range
+from geometry_msgs.msg import Twist
 
 import tf
 import math
 import time
 
 exec_time = 1/25;
+
+ThrottleChannel = 2
+SteeringChannel = 0
+
+throttleTrimPWM = 1500	#trim values
+steeringTrimPWM = 1500
+throttleMultPWM = 60 	#multiplication params
+steeringMultPWM = 60
+
+RCOverride = [1500, 1500, 1000, 1500, 1000, 1500, 1500, 1500]
+
+rangeUltrasonicFront = 4
+rangeUltrasonicBack  = 4
 
 def setMode():
 	print("Set mode")
@@ -46,31 +61,68 @@ def disarm():
 	except rospy.ServiceException as e:
 		print("Service call failed: %s" %e)
 
-def subsIMU():
-	rospy.Subscriber("/mavros/imu/data", Imu, getIMU)
-	rospy.spin()
-
-def subsRCIN():
-	rospy.Subscriber("/mavros/rc/in", RCIn, getRC)
-	rospy.spin()
-
-def subsUltrasonic():
+def subscribeToData():
+	print("Subscribing")
+	#rospy.Subscriber("/mavros/imu/data", Imu, getIMU)
+	#rospy.Subscriber("/mavros/rc/in", RCIn, getRC)
+	rospy.Subscriber("/ultrasound1", Range, getRange1)
+	#rospy.Subscriber("/ultrasound2", Range, getRange2)
+	rospy.Subscriber("/turtle1/cmd_vel", Twist, getTeleop)
 
 def getIMU(data):
 	print(data);
 
 def getRC(data):
-	#pub = rospy.Publisher('/mavros/rc/override', OverrideRCIn, queue_size=1)
-    	#rate = rospy.Rate(10) # 10hz
 	print(data.channels)
+
+def getRange1(data):
+	global rangeUltrasonicFront
+	rangeUltrasonicFront = data.range
+
+def getRange2(data):
+	global rangeUltrasonicBack
+	rangeUltrasonicBack = data.range
+
+def getTeleop(data):
+	setRCOverrideTeleop(data.linear.x, data.angular.z)
+
+def setRCOverrideTeleop(throttleInput, steeringInput):
+	pub = rospy.Publisher('/mavros/rc/override', OverrideRCIn, queue_size=1)
+	rate = rospy.Rate(10)
+	flag = True
+	RCOverride[ThrottleChannel] = throttleTrimPWM - int(throttleInput)*throttleMultPWM
+	RCOverride[SteeringChannel] = steeringTrimPWM - int(steeringInput)*steeringMultPWM
+
+	#if obstacle is too close, avoid forward/backward movement
+	if(rangeUltrasonicFront < 0.6):
+		if(RCOverride[ThrottleChannel] < 1500):
+			RCOverride[ThrottleChannel] = 1500
+
+	if(rangeUltrasonicBack < 0.6):
+		if(RCOverride[ThrottleChannel] > 1500):
+			RCOverride[ThrottleChannel] = 1500
+
+	while not rospy.is_shutdown() and flag:
+		flag = False
+		pub.publish(RCOverride)
+		#pub.publish(RCOverride)
+		rate.sleep()
+
+def setRCArm():
+	pub = rospy.Publisher('/mavros/rc/override', OverrideRCIn, queue_size=1)
+	RCOverride[ThrottleChannel] = 1000
+	pub.publish(RCOverride)
 
 if __name__ == '__main__':
 	#try:
 		print("Program starts")
 		rospy.init_node('semiAutoControl', anonymous=True)
-		#arm()
+		#setRCArm()
+		arm()
 		#time.sleep(3)
-		subsRCIN()
+		subscribeToData()
+		rospy.spin() #prevent from exiting
+
 
 	#except rospy.ROSInterrupException:
 	#	pass
